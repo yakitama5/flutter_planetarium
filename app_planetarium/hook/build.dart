4b57fccd46dd3d1dae6487fb194516badb12d3cf
Yakuran <rittitarou@yahoo.co.jp>
2025-12-06 01:11:42 +0900

    🚧wip: 全体に散りばめる星を作成

--- app_planetarium/hook/build.dart
+++ app_planetarium/hook/build.dart
@@ -6,13 +6,20 @@ void main(List<String> args) {
     buildModels(
       buildInput: config,
       inputFilePaths: [
-        'assets/glb/cubit.glb',
         'assets/glb/earth.glb',
         'assets/glb/four_pointed_star.glb',
+        'assets/glb/jupiter.glb',
+        'assets/glb/mars.glb',
+        'assets/glb/mercury.glb',
+        'assets/glb/moon.glb',
+        'assets/glb/neptune.glb',
         'assets/glb/pentagram.glb',
         'assets/glb/polygonal_star.glb',
+        'assets/glb/saturn.glb',
         'assets/glb/star_dome.glb',
         'assets/glb/sun.glb',
+        'assets/glb/uranus.glb',
+        'assets/glb/venus.glb',
       ],
     );
   });
--- app_planetarium/lib/models.dart
+++ app_planetarium/lib/models.dart
@@ -1,12 +1,19 @@
 /// 利用可能な3Dモデルの一覧
 enum Models {
-  cubit('build/models/cubit.model'),
-  earth('build/models/earth.model', unlit: true),
-  fourPointedStar('build/models/four_pointed_star.model'),
-  pentagram('build/models/pentagram.model'),
-  polygonalStar('build/models/polygonal_star.model'),
-  starDome('build/models/star_dome.model'),
-  sun('build/models/sun.model');
+  earth('build/models/earth.glb.model'),
+  fourPointedStar('build/models/four_pointed_star.glb.model'),
+  jupiter('build/models/jupiter.glb.model'),
+  mars('build/models/mars.glb.model'),
+  mercury('build/models/mercury.glb.model'),
+  moon('build/models/moon.glb.model'),
+  neptune('build/models/neptune.glb.model'),
+  pentagram('build/models/pentagram.glb.model'),
+  polygonalStar('build/models/polygonal_star.glb.model'),
+  saturn('build/models/saturn.glb.model'),
+  starDome('build/models/star_dome.glb.model'),
+  sun('build/models/sun.glb.model'),
+  uranus('build/models/uranus.glb.model'),
+  venus('build/models/venus.glb.model');
 
   final String path;
   final bool unlit;
